52a587826557f1b2967acccc15db1a4b268805d7
J. Austin Hughey <jaustinhughey@gmail.com>
2016-06-06 23:02:30 -0500

    beef up the glances installer

--- installers/mac-glances.sh
+++ installers/mac-glances.sh
@@ -1,7 +1,58 @@
 #!/bin/bash
 
-mkdir -p "$HOME/.opt/python3"
 brew install python3
 pip3 install --upgrade pip
+
 export PYTHONUSERBASE="$HOME/.opt/python3"
+export INSTALLDIR=$PYTHONUSERBASE
+export MYBIN="$HOME/.bin"
+
+echo "Creating user-specific install directory: $INSTALLDIR"
+
+mkdir -p $INSTALLDIR
+
+echo "Creating personal bin/ dir: $MYBIN"
+
+mkdir -p $MYBIN
+
+echo "Installing packages via pip3..."
+
 pip3 install --user psutil setuptools bottle pysnmp zeroconf netifaces influxdb statsd pystache matplotlib pika bernhard py-cpuinfo scandir glances
+
+echo ""
+echo ":::::::::::::"
+echo ""
+echo "Install appears complete; now trying to symlink installed"
+echo "application launch scripts into your MYBIN directory..."
+echo ""
+
+ln -s $INSTALLDIR/bin/* $MYBIN/
+
+glances --version
+
+echo ""
+echo ""
+echo ""
+echo ""
+echo "WARNING: You absolutely MUST set the environment variable"
+echo ""
+echo "  PYTHONUSERBASE"
+echo ""
+echo "...in your shell of choice. Its value must be equal to:"
+echo "  $PYTHONUSERBASE"
+echo ""
+echo "For example, in ~/.zshrc:"
+echo ""
+echo "    export PYTHONUSERBASE=\"$PYTHONUSERBASE\""
+echo ""
+echo "Example in config.fish:"
+echo ""
+echo "    set -gx PYTHONUSERBASE \"$PYTHONUSERBASE\""
+echo ""
+echo ""
+echo ""
+echo ""
+
+# Unset the above-declared variables now that we're done
+unset INSTALLDIR
+unset MYBIN
